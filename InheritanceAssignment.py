c135e59e2415b44a4522d252ee26a7dc8002d9f5
JamRaid36 <105257619+JamRaid36@users.noreply.github.com>
2022-08-16 21:42:09 -0700

    uploaded Inheritance Submission Assignment
    Created a class and 2 child classes that inherit from the parent but that add unique properties to each specific child.

---
+++ InheritanceAssignment.py
@@ -0,0 +1,25 @@
+
+
+
+class Guitar: #This creates a parent class of guitar
+    brand = 'No Brand Provided'
+    model = ' '
+    color = ' '
+    year = ' '
+
+
+# This is a specific type of guitar that has many of
+#of the same attributes of all guitars but is unique
+# in it's operation
+
+class Electric(Guitar): 
+    pickups = ' '
+    switch = ' '
+
+# This is a specific type of guitar that has many of
+#of the same attributes of all guitars but is unique
+
+class Acoustic(Guitar):
+    style = ' '
+    tonewood = ' '
+    acoustic/electric = ' '
